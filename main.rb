000a1772f6dfe500ae86be11b5532851b5c63070
Neelp22 <neelp22@gmail.com>
2013-09-16 16:47:11 -0400

    takethree

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require 'sinatra/reloader'
 require 'active_support/all'
 require 'active_record'
 
-ActiveRecord::Base.establish_connection(ENV['HEROKU_POSTGRESQL_BROWN_URL'] 
+ActiveRecord::Base.establish_connection(ENV['HEROKU_POSTGRESQL_BROWN_URL']) 
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
